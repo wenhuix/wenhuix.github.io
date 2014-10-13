cbed602e9803e80053b8b2bb8ee847624e6b0d7b
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-13 17:00:03 +0800

    first commit

---
+++ js/comment.js
@@ -0,0 +1,51 @@
+// This code goes ABOVE the main HTML Comment Box code!
+// replace the text in the single quotes below to customize labels.
+hcb_user = {
+    //L10N
+    comments_header : '评论',
+    name_label : '称呼',
+    content_label: '在这里输入你的评论',
+    submit : '提交',
+    logout_link : '<img title="log out" src="//www.htmlcommentbox.com/static/images/door_out.png" alt="[logout]" class="hcb-icon"/>',
+    admin_link : '<img src="//www.htmlcommentbox.com/static/images/door_in.png" alt="[login]" class="hcb-icon"/>',
+    no_comments_msg: '还没有人评论呢，来坐沙发吧！',
+    add:'Add your comment',
+    again: '再次评论：',
+    rss:' ',
+    said:'说：',
+    prev_page:'<img src="//www.htmlcommentbox.com/static/images/arrow_left.png" class="hcb-icon" title="previous page" alt="[prev]"/>',
+    next_page:'<img src="//www.htmlcommentbox.com/static/images/arrow_right.png" class="hcb-icon" title="next page" alt="[next]"/>',
+    showing:'Showing',
+    to:'to',
+    website_label:'website (optional)',
+    email_label:'email',
+    anonymous:'匿名',
+    mod_label:'(mod)',
+    subscribe:'email me replies',
+    are_you_sure:'Do you want to flag this comment as inappropriate?',
+    
+    reply:'<img src="//www.htmlcommentbox.com/static/images/reply.png"/> reply',
+    flag:'<img src="//www.htmlcommentbox.com/static/images/flag.png"/> flag',
+    like:'<img src="//www.htmlcommentbox.com/static/images/like.png"/> like',
+    
+    //dates
+    days_ago:'天之前',
+    hours_ago:'个小时之前',
+    minutes_ago:'分钟之前',
+    within_the_last_minute:'少于一分钟',
+
+    msg_thankyou:'感谢你的评论！',
+    msg_approval:'(this comment is not published until approved)',
+    msg_approval_required:'Thank you for commenting! Your comment will appear once approved by a moderator.',
+    
+    err_bad_html:'Your comment contained bad html.',
+    err_bad_email:'Please enter a valid email address.',
+    err_too_frequent:'You must wait a few seconds between posting comments.',
+    err_comment_empty:'Your comment was not posted because it was empty!',
+    err_denied:'Your comment was not accepted.',
+
+    //SETTINGS
+    MAX_CHARS: 2048,
+    PAGE:'', // ID of the webpage to show comments for. defaults to the webpage the user is currently visiting.
+    RELATIVE_DATES:true // show dates in the form "X hours ago." etc.
+};
---
+++ js/footer.js
@@ -0,0 +1,15 @@
+
+/*footer*/
+
+document.write("<div>");
+    document.write("<ul class=\"footer_menu_list\">");
+        document.write("<li>友情链接：</li>");
+        document.write("<li><a href=\"http://www.ustc.edu.cn\">中国科大</a></li>");
+        document.write("<li><a href=\"http://www.arcsoft.com.cn/\">虹软中国</a></li>");
+        document.write("<li><a href=\"https://github.com/\" >GitHub</a></li>");			
+    document.write("</ul>");
+document.write("</div>");
+
+document.write("<div class=\"copyright\">");
+    document.write("Copyright © 2014 BY Wenhuix | Email:<a href=\"mailto:xiangwenhui@gmail.com\" class=\"email\">littlewest@foxmail.com</a>");
+document.write("</div>");

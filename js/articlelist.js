70469e2a0c58bc4ea4ae3d8a6879af3f03b035a3
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-17 16:40:21 +0800

    加入文章列表

---
+++ js/articlelist.js
@@ -0,0 +1,7 @@
+﻿document.write("<div id=\"column_w300\">");
+document.write("<div class=\"header_03\">文章列表<\/div>");
+	document.write("<div class=\"column_w300_section_01\">");
+		document.write("<p\"><a href=\"http://wenhuix.github.io\/research\/FunnyIP.html\">好玩的图像处理算法<\/a> 2014-10-17<\/p>");
+	document.write("<\/div>");
+document.write("<div class=\"cleaner\"><\/div>");
+document.write("<\/div>");

a9d0fcd5b925e16f23e5ea56e3f00bb18591dbff
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-19 22:38:16 +0800

    加入博客

---
+++ js/bloglist.js
@@ -0,0 +1,7 @@
+﻿document.write("<div id=\"column_w260\">");
+document.write("<div class=\"header_03\">文章列表<\/div>");
+	document.write("<div class=\"column_w300_section_01\">");
+		document.write("<p\"><a href=\"http://wenhuix.github.io\/blog\/hzmls.html\">杭州马拉松<\/a> 2014-10-19<\/p>");
+	document.write("<\/div>");
+document.write("<div class=\"cleaner\"><\/div>");
+document.write("<\/div>");

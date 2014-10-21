26cf228072d5b61cda8576c091aa10dd10d24a45
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-21 13:01:39 +0800

    加入文献检索网站

--- js/researchlist.js
+++ js/researchlist.js
@@ -1,7 +1,8 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/research\/FunnyIP.html\">好玩的图像处理算法<\/a> 2014-10-17<\/p>");
+		document.write("<p\"><a href=\"http://wenhuix.github.io\/research\/webpaper.html\">计算机视觉论文检索<\/a> 2014-10-21<\/p>");
+		document.write("<p\"><a href=\"http://wenhuix.github.io\/research\/FunnyIP.html\">好玩的图像处理算法<\/a> 2014-10-17<\/p>");		
 	document.write("<\/div>");
 document.write("<div class=\"cleaner\"><\/div>");
 document.write("<\/div>");

3b323e5171b58f628733da08477f98f842314917
wenhuix <xwh1@mail.ustc.edu.cn>
2014-10-21 15:05:10 +0800

    加入访问计数器

--- js/footer.js
+++ js/footer.js
@@ -6,10 +6,11 @@ document.write("<div>");
         document.write("<li>友情链接：</li>");
         document.write("<li><a href=\"http://www.ustc.edu.cn\">中国科大</a></li>");
         document.write("<li><a href=\"http://www.arcsoft.com.cn/\">虹软中国</a></li>");
-        document.write("<li><a href=\"https://github.com/\" >GitHub</a></li>");			
+        document.write("<li><a href=\"https://github.com/\" >GitHub</a></li>");	
+		document.write("<li>访问次数：</li>");
+		document.write("<li><a href=\"http://www.amazingcounters.com\"><img border=\"0\" src=\"http://cc.amazingcounters.com/counter.php?i=3179596&c=9539101\" alt=\"AmazingCounters.com\"></a></li>");			
     document.write("</ul>");
 document.write("</div>");
-
 document.write("<div class=\"copyright\">");
     document.write("Copyright © 2014 BY Wenhuix | Email:<a href=\"mailto:littlewest@foxmail.com\" class=\"email\">littlewest@foxmail.com</a>");
 document.write("</div>");

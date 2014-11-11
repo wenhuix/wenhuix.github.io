c834d11d72aeeb8aed537a583714f5e56664a9c5
wenhuix <xwh1@mail.ustc.edu.cn>
2014-11-11 20:43:22 +0800

    加入骑行绍兴

--- js/bloglist.js
+++ js/bloglist.js
@@ -2,6 +2,7 @@
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
 		document.write("<p\"><a href=\"http://wenhuix.github.io\/blog\/hzmls.html\">杭州马拉松<\/a> 2014-11-05<\/p>");
+		document.write("<p\"><a href=\"http://wenhuix.github.io\/blog\/hz2sx.html\">骑行绍兴<\/a> 2014-10-07<\/p>");
 	document.write("<\/div>");
 document.write("<div class=\"cleaner\"><\/div>");
 document.write("<\/div>");

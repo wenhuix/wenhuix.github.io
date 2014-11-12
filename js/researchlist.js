327715cebb4bbfdbc1a9f37d12339c6adf6460d2
wenhuix <xwh1@mail.ustc.edu.cn>
2014-11-12 19:19:42 +0800

    加入计算机视觉库列表

--- js/researchlist.js
+++ js/researchlist.js
@@ -1,6 +1,7 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
+		document.write("<p\">2014-11-12 <a href=\"http://wenhuix.github.io\/research\/cvlib.html\">计算机视觉库<\/a><\/p>");
 		document.write("<p\">2014-10-21 <a href=\"http://wenhuix.github.io\/research\/webpaper.html\">计算机视觉论文检索<\/a><\/p>");
 		document.write("<p\">2014-10-17 <a href=\"http://wenhuix.github.io\/research\/FunnyIP.html\">有趣的图像处理算法<\/a><\/p>");		
 	document.write("<\/div>");

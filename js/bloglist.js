2317c7a71c441a00be39ccdd3359bb9a4a52bac4
wenhuix <xwh1@mail.ustc.edu.cn>
2014-11-12 19:03:40 +0800

    修改文字和时间的顺序

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,8 +1,8 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/blog\/hzmls.html\">杭州马拉松<\/a> 2014-11-05<\/p>");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/blog\/hz2sx.html\">骑行绍兴<\/a> 2014-10-07<\/p>");
+		document.write("<p\">2014-11-05 <a href=\"http://wenhuix.github.io\/blog\/hzmls.html\">杭州马拉松<\/a><\/p>");
+		document.write("<p\">2014-10-07 <a href=\"http://wenhuix.github.io\/blog\/hz2sx.html\">骑行绍兴<\/a><\/p>");
 	document.write("<\/div>");
 document.write("<div class=\"cleaner\"><\/div>");
 document.write("<\/div>");
--- js/projectlist.js
+++ js/projectlist.js
@@ -1,9 +1,9 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/project\/comdbg.html\">串口调试助手<\/a> 2014-10-21<\/p>");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/project\/xunfeivoice.html\">快递员助手<\/a> 2014-03-16<\/p>");
-		document.write("<p\"><a href=\"http://wenhuix.github.io\/project\/motris.html\">智能交通中的车辆跟踪<\/a> 2012-09-10<\/p>");
+		document.write("<p\">2014-10-21 <a href=\"http://wenhuix.github.io\/project\/comdbg.html\">串口调试助手<\/a><\/p>");
+		document.write("<p\">2014-03-16 <a href=\"http://wenhuix.github.io\/project\/xunfeivoice.html\">快递员助手<\/a><\/p>");
+		document.write("<p\">2012-09-10 <a href=\"http://wenhuix.github.io\/project\/motris.html\">智能交通中的车辆跟踪<\/a><\/p>");
 	document.write("<\/div>");
 document.write("<div class=\"cleaner\"><\/div>");
 document.write("<\/div>");

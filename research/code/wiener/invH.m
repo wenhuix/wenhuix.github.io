dd7e40cbb3a3a6e671eaac9736c3d64a57b079c8
wenhuix <xwh1@mail.ustc.edu.cn>
2014-11-22 13:03:06 +0800

    修改图像复原

--- research/code/wiener/invH.m
+++ research/code/wiener/invH.m
@@ -1,9 +1,32 @@
 N=256;
-n=.2;
+n=.0002;
 % read image
 f = double(imread('lenna.tif'));
-figure;imshow(x,gray(256));title('orig');
+figure;imshow(f,gray(256));title('orig');
 
+% blur and add noise
+b=ones(4,4)/4^2;
+F=fft2(f);
+B=fft2(b,N,N);
+G=F.*B;
+g=ifft2(G);
+G=fft2(g);
+figure; imshow(ifft2(G),gray(256));title('blur');
+
+imwrite(uint8(ifft2(G)),'blur.jpg');
+
+% inverse filter
+B(abs(B)<n)=n;
+H=ones(N,N)./B;
+I=G.*H;
+im=abs(ifft2(I));
+
+figure; imshow(im,gray(256));title('blur Result');
+imwrite(uint8(im),'blur_res.jpg');
+
+MSE = mean(mean((double(im) - double(f)).^2,2),1)
+
+n=1;
 % blur and add noise
 b=ones(4,4)/4^2;
 F=fft2(f);
@@ -13,10 +36,15 @@ g=ifft2(G)+10*randn(N,N);
 G=fft2(g);
 figure; imshow(ifft2(G),gray(256));title('Add noise and blur');
 
+imwrite(uint8(ifft2(G)),'blur_noise.jpg');
+
 % inverse filter
 B(abs(B)<n)=n;
 H=ones(N,N)./B;
 I=G.*H;
 im=abs(ifft2(I));
 
-figure; imshow(im,gray(256));title('Result');
+figure; imshow(im,gray(256));title('blur&noise Result');
+imwrite(uint8(im),'blur_noise_res.jpg');
+
+MSE = mean(mean((double(im) - double(f)).^2,2),1)

425e309639a5efee7fbbdabb0c90f1d0136704a4
wenhuix <xwh1@mail.ustc.edu.cn>
2014-11-24 17:34:48 +0800

    加入百度统计

--- js/footer.js
+++ js/footer.js
@@ -14,3 +14,8 @@ document.write("</div>");
 document.write("<div class=\"copyright\">");
     document.write("Copyright © 2014 BY Wenhuix | Email:<a href=\"mailto:littlewest@foxmail.com\" class=\"email\">littlewest@foxmail.com</a>");
 document.write("</div>");
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6b5f4352b64ebec3e9a0e16c1e6c19e6' type='text/javascript'%3E%3C/script%3E"));
+</script>

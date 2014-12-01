65e76f57868632cd2e80202ff09f8c32479ef681
wenhuix <xwh1@mail.ustc.edu.cn>
2014-12-01 11:01:52 +0800

    改标题

--- js/researchlist.js
+++ js/researchlist.js
@@ -1,7 +1,7 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
-		document.write("<p\">2014-11-21 <a href=\"http://wenhuix.github.io\/research\/denoise.html\">图像去噪算法<\/a><\/p>");
+		document.write("<p\">2014-11-21 <a href=\"http://wenhuix.github.io\/research\/denoise.html\">图像去噪算法：NL-Means和BM3D<\/a><\/p>");
 		document.write("<p\">2014-11-14 <a href=\"http://wenhuix.github.io\/research\/onlinetools.html\">实用在线工具<\/a><\/p>");
 		document.write("<p\">2014-11-12 <a href=\"http://wenhuix.github.io\/research\/cvlib.html\">计算机视觉库<\/a><\/p>");
 		document.write("<p\">2014-10-21 <a href=\"http://wenhuix.github.io\/research\/webpaper.html\">计算机视觉论文检索<\/a><\/p>");

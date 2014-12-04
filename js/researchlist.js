81d46668f99de927cd9911d22c08944235324849
wenhuix <wenhuix@qq.com>
2014-12-04 17:56:33 +0800

    加入从现实出发谈代码管理

--- js/researchlist.js
+++ js/researchlist.js
@@ -1,6 +1,8 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
+	
+		document.write("<p\">2014-11-14 <a href=\"http://wenhuix.github.io\/research\/codeStyle.html\">从现实出发谈代码管理<\/a><\/p>");
 		document.write("<p\">2014-11-21 <a href=\"http://wenhuix.github.io\/research\/denoise.html\">图像去噪算法：NL-Means和BM3D<\/a><\/p>");
 		document.write("<p\">2014-11-14 <a href=\"http://wenhuix.github.io\/research\/onlinetools.html\">实用在线工具<\/a><\/p>");
 		document.write("<p\">2014-11-12 <a href=\"http://wenhuix.github.io\/research\/cvlib.html\">计算机视觉库<\/a><\/p>");

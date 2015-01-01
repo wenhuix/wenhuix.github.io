6df0cb25a14494e3bd825c1c4f5f8d5fb23b0ff0
wenhuix <xwh1@mail.ustc.edu.cn>
2015-01-02 01:19:55 +0800

    加入心肺复苏术

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,6 +1,7 @@
 ﻿document.write("<div id=\"column_w260\">");
 document.write("<div class=\"header_03\">文章列表<\/div>");
 	document.write("<div class=\"column_w300_section_01\">");
+		document.write("<p\">2015-01-01 <a href=\"http://wenhuix.github.io\/blog\/cpr.html\">心肺复苏术<\/a><\/p>");
 		document.write("<p\">2014-11-05 <a href=\"http://wenhuix.github.io\/blog\/hzmls.html\">杭州马拉松<\/a><\/p>");
 		document.write("<p\">2014-10-07 <a href=\"http://wenhuix.github.io\/blog\/hz2sx.html\">骑行绍兴<\/a><\/p>");
 	document.write("<\/div>");

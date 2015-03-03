3ebef8b21ae89754dfd9bd6d9affb10ca04609ff
wenhuix <wenhuix@qq.com>
2015-03-03 17:04:54 +0800

    update copyright

--- js/footer.js
+++ js/footer.js
@@ -6,13 +6,12 @@ document.write("<div>");
         document.write("<li>友情链接：</li>");
         document.write("<li><a href=\"http://www.ustc.edu.cn\"  target=\"_blank\">中国科大</a></li>");
         document.write("<li><a href=\"http://www.arcsoft.com.cn/\" target=\"_blank\">虹软中国</a></li>");
-        document.write("<li><a href=\"https://github.com/\"  target=\"_blank\">GitHub</a></li>");	
-		document.write("<li>访问次数：</li>");
+        document.write("<li><a href=\"https://github.com/\"  target=\"_blank\">GitHub</a></li>");
 		document.write("<li><a href=\"http://www.amazingcounters.com\" target=\"_blank\"><img border=\"0\" src=\"http://cc.amazingcounters.com/counter.php?i=3179596&c=9539101\" alt=\"AmazingCounters.com\"></a></li>");			
     document.write("</ul>");
 document.write("</div>");
 document.write("<div class=\"copyright\">");
-    document.write("Copyright © 2014 BY Wenhuix | Email:<a href=\"mailto:littlewest@foxmail.com\" class=\"email\">littlewest@foxmail.com</a>");
+    document.write("Copyright © 2014-2015 BY Wenhuix | Email:<a href=\"mailto:littlewest@foxmail.com\" class=\"email\">littlewest@foxmail.com</a>");
 document.write("</div>");
 
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
